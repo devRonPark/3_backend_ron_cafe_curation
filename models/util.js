3a1c401b56773ab1363d7e9c3fb4a58af59b7afe
devPark <ronny_park@naver.com>
2022-02-14 19:20:22 +0900

    카페 위치 데이터 좌표계 변환 함수 작성

--- models/util.js
+++ models/util.js
@@ -2,7 +2,10 @@
 const colors = require('colors');
 // 콘솔 창에 현재 날짜 및 시간 출력
 const moment = require('moment');
+// 랜덤한 토큰을 생성하기 위해 사용
 const crypto = require('crypto');
+// 카페의 위치 데이터 값 중부원점 >> WGS84 좌표계로 변환
+const proj4 = require('proj4');
 require('moment-timezone');
 // 시간대는 한국 서울 기준
 moment.tz.setDefault('Asia/Seoul');
@@ -27,3 +30,18 @@ exports.generateRandomNumber = function (min, max) {
 exports.generateRandomToken = function () {
   return crypto.randomBytes(20).toString('hex');
 };
+const convertLocationData = coordinateObj => {
+  //GRS80(중부원점) 좌표계
+  const grs80 =
+    '+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs';
+  //wgs84(위경도)좌표계
+  const wgs84 =
+    '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees';
+  // let coord = proj4(fromProjection, toProjection, coordinates);
+  // fromProjection : 변환 대상 좌표계, toProjection : 변환 목표 좌표계, coordinates : 객체 또는 배열 형태 (예시 {x: 'x', y: 'y'} 또는 [x, y])
+  const coord = proj4(grs80, wgs84, coordinateObj);
+  console.log('coord: ', coord);
+  return { latitude: coord.x, longitude: coord.y };
+};
+// 예시
+convertLocationData({ x: 199901.976388039, y: 455159.34378238 });
