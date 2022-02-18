06306535272beab30775f996796b572bc40e7d13
devPark <ronny_park@naver.com>
2022-02-18 13:33:58 +0900

    모든 요청에 대한 쿼리문 결과 반환, err.stack 로거로 남기기

--- config/mysql.js
+++ config/mysql.js
@@ -10,29 +10,34 @@ const DB = async (type, sql, params) => {
     // state : 쿼리문 실행 성공 시 true
     // error : 쿼리문 error 정보 반환
     let result = {}; // 반환 값
+    // connection 생성
     const connection = await pool.getConnection(async conn => conn);
     try {
       let executedSql;
       let rows;
       if (params) {
-        [rows] = await connection.query(sql, params);
+        // 콘솔에 출력할 sql문 생성
         executedSql = mysql.format(sql, params);
+        [rows] = await connection.query(sql, params);
+        console.log('results: ', rows);
       } else {
-        [rows] = await connection.query(sql);
+        // 콘솔에 출력할 sql문 생성
         executedSql = mysql.format(sql);
+        [rows] = await connection.query(sql);
+        console.log('results: ', rows);
       }
-      printSqlLog(executedSql);
+      printSqlLog(executedSql); // 현재 실행되는 sql문 콘솔에 출력
 
-      if (type == 'GET') result.data = rows;
+      result.data = rows; // 조회되는 데이터 추가
       result.state = true;
       connection.release(); // 사용된 풀 반환
       return result;
     } catch (err) {
-      logger.error(`Query Error: ${err.message}`);
+      logger.error(`Query Error: ${err.stack}`);
       throw new Error(err.message);
     }
   } catch (err) {
-    logger.error(`DB Error: ${err.message}`);
+    logger.error(`DB Error: ${err.stack}`);
     throw new Error(err.message);
   }
 };
