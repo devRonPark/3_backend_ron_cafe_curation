3b10ba801907dd0519bc4b75f6d62d15c5296d25
devPark <ronny_park@naver.com>
2022-02-18 12:48:30 +0900

    댓글 조회, 등록, 수정, 삭제  라우트 수정

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -3,15 +3,39 @@ const CommentController = require('../controllers/comment.controllers');
 const commentRouter = express.Router();
 const {
   validateComment,
-  isCafeIdExist,
-  isUserIdExist,
-} = require('../middlewares/middlewares');
-
+  validateCallback,
+  isCafeIdValidate,
+  isUserIdValidate,
+  isIdParamValidate,
+  isCommentIdValidate,
+} = require('../middlewares/validate');
+// 모든 사용자 한줄평 조회
+commentRouter.get('/', CommentController.getAllComments);
+// 카페 별 사용자 한줄평 조회
+commentRouter.get(
+  '/:id',
+  isIdParamValidate,
+  CommentController.getCommentsOfCafe,
+);
 // 사용자 한줄평 작성
+// /comments?cafeId=cafeId
 commentRouter.post(
-  '/comments',
-  [isUserIdExist, isCafeIdExist, validateComment, validateCallback],
+  '/',
+  [isCafeIdValidate, isUserIdValidate, validateComment, validateCallback],
   CommentController.createComment,
 );
 // 사용자 한줄평 수정
-commentRouter.put('/comments/:id', CommentController.editComment);
+// /comments/3
+commentRouter.put(
+  '/:id',
+  [isCommentIdValidate, validateComment, validateCallback],
+  CommentController.editComment,
+);
+// 사용자 한줄평 삭제
+// /comments/3
+commentRouter.delete(
+  '/:id',
+  isCommentIdValidate,
+  CommentController.deleteComment,
+);
+module.exports = commentRouter;
